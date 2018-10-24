6a30c7cd9a14ae84e50c50147e025d0dc0766970
jismariya97 <44168203+jismariya97@users.noreply.github.com>
2018-10-24 20:15:36 +0530

    cprgm

---
+++ format.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main()
+{
+int a,b;
+float c;
+char n;
+printf("enter 3 nums");
+scanf("%2d %d %f",&a,&b,&c);
+printf("a=%-05d b=%d c=%f\n",a,b,c);
+printf("entr a char");
+scanf("%c",&n);
+printf(n);
+}
+
+
